f61aa5ace53a6b704404452d79122d88b68512fd
Elliot-Os <osnathan6@gmail.com>
2023-02-22 05:36:31 -0800

    hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - Start the program
+* Description: prints the string inside of it
+* Return: it's return 0 (Success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

2987cb9d1b5004b3dc03daad0fe0f2477a537c60
Elliot-Os <osnathan6@gmail.com>
2023-03-20 12:56:35 -0700

    updated

---
+++ 0x0C-more_malloc_free/3-main.c
@@ -0,0 +1,26 @@
+
+        }
+        if (!(i % 10) && i)
+        {
+            printf("\n");
+        }
+        printf("0x%02x", buffer[i]);
+        i++;
+    }
+    printf("\n");
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int *a;
+
+    a = array_range(0, 10);
+    simple_print_buffer(a, 11);
+    free(a);
+    return (0);
+}

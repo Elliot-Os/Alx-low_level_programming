862622868c67ac0ae545ab81af8dd27e7fc4e0de
Elliot-Os <osnathan6@gmail.com>
2023-04-09 16:45:50 -0700

    updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,34 +1,35 @@
 #include <fcntl.h>
+#include <stddef.h>
 #include <unistd.h>
+#include <string.h>
 /**
- * append_text_to_file - appending text into a file
- * @filename: the file
- * @text_content: text to append.
- * Return: 1 on success and -1 on failure
+ * create_file - function that creates a file.
+ * @filename: the file we will create
+ * @text_content: content of the file
+ * Return: 1 on success, -1 on failure
 */
-int append_text_to_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	int file;
-	ssize_t wsize, len = 0;
+	ssize_t len, wsize;
 
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		while (text_content[len])
-			len++;
-	}
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
-	file = open(filename, O_WRONLY | O_APPEND);
 	if (file == -1)
 		return (-1);
 
-	wsize = write(file, text_content, len);
-	if (wsize == -1)
+	if (text_content != NULL)
 	{
-		close(file);
-		return (-1);
+		len = strlen(text_content);
+		wsize = write(file, text_content, len);
+		if (wsize == -1 || (ssize_t)wsize != len)
+		{
+			close(file);
+			return (-1);
+		}
 	}
 
 	close(file);
